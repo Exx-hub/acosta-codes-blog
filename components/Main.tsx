2965a4a961cd806ea562b0aa93207dcca38f4c81
Alvin Acosta <alvin.acosta@movon.com.ph>
2022-12-08 17:12:11 +0800

    fix apostrophe issue

--- components/Main.tsx
+++ components/Main.tsx
@@ -3,8 +3,9 @@ function Main() {
     <div>
       <h1>ALVIN ACOSTA</h1>
       <p>
-        I'm a Manila-based full-time ReactJs Developer..Freelance Fullstack
-        Developer.
+        {
+          "I'm a Manila-based full-time ReactJs Developer..Freelance Fullstack Developer."
+        }
       </p>
       {/* <p>Welcome to my Blog! You can also view my Portfolio here.</p> */}
       <div>socials</div>
